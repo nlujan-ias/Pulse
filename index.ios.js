75aacc67e895655a0389cac92eafb7861ee41af6
Naim Lujan <nlujan@Integraladmins-MacBook-Pro-16.local>
2015-12-16 17:34:54 -0500

    created index with routing

--- index.ios.js
+++ index.ios.js
@@ -5,49 +5,19 @@
 'use strict';
 
 var React = require('react-native');
-var {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-} = React;
+var Pulse = require('./Pulse');
+var {View, Text, StyleSheet, TouchableOpacity, AppRegistry, TouchableHighlight} = React;
 
-var Pulse = React.createClass({
+var Index = React.createClass({
   render: function() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to Pulse!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <React.NavigatorIOS
+        initialRoute={{
+          title: 'Hello',
+          component: Pulse,
+        }}/>
     );
   }
 });
 
-var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
-AppRegistry.registerComponent('Pulse', () => Pulse);
+AppRegistry.registerComponent('Pulse', () => Index);
